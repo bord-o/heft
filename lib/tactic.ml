05ec6534a07617c46f50173a341dc1927408ab80
Brody Little <brodylittle011@gmail.com>
2025-12-29 14:45:17 -0800

    bk

--- lib/tactic.ml
+++ lib/tactic.ml
@@ -3,37 +3,27 @@ open Derived
 open Effect.Deep
 open Printing
 
-type goal = term list * term
-[@@deriving show]
+type goal = term list * term [@@deriving show]
+
 let gconcl = snd
 
-type _ Effect.t += 
-  | Subgoals : goal list -> thm list Effect.t
-
-type tactic_name = 
-  | Assumption
-  | Conj
-
-type command =
-  | SetGoal of goal
-  | Apply of tactic_name
-  | ShowGoal
-
+type _ Effect.t += Subgoals : goal list -> thm list Effect.t
+type tactic_name = Assumption | Conj | Refl
+type command = SetGoal of goal | Apply of tactic_name | ShowGoal
 type tactic = goal -> thm
 
 (* Exception for kernel errors *)
 exception Kernel_error of kernel_error
+exception Subgoals_not_ready
 
 (* Helper to convert Result to exception *)
-let unwrap_result = function
-  | Ok x -> x
-  | Error e -> raise (Kernel_error e)
+let unwrap_result = function Ok x -> x | Error e -> raise (Kernel_error e)
 
-(* Example tactics - all return thm *)
+(* Example tactics *)
 let conj_tac (asms, concl) =
   let p, q = destruct_conj concl in
-  match Effect.perform (Subgoals [(asms, p); (asms, q)]) with
-  | [p_thm; q_thm] -> conj p_thm q_thm |> unwrap_result
+  match Effect.perform (Subgoals [ (asms, p); (asms, q) ]) with
+  | [ p_thm; q_thm ] -> conj p_thm q_thm |> unwrap_result
   | _ -> failwith "expected both sides of conj"
 
 let assumption_tac (asms, concl) =
@@ -41,84 +31,169 @@ let assumption_tac (asms, concl) =
   | Some asm -> assume asm |> unwrap_result
   | None -> failwith "no matching assumption"
 
+let refl_tac : tactic =
+ fun (_asms, concl) ->
+  match destruct_eq concl with
+  | Ok (l, r) when alphaorder l r = 0 -> refl l |> Result.get_ok
+  | Ok _ -> failwith "REFL_TAC: sides of equality not identical"
+  | Error _ -> failwith "REFL_TAC: goal not an equalit"
+
 let get_tactic = function
   | Assumption -> assumption_tac
   | Conj -> conj_tac
+  | Refl -> refl_tac
 
-(* Handler that processes commands *)
-let rec solve_with_commands (commands : command list ref) tactic goal =
+(* Proof state *)
+type tactic_application = { tactic : tactic_name; goal : goal }
+
+type proof_session = {
+  mutable goal_stack : goal list;
+  mutable proof_script : tactic_application list;
+  mutable cache : (goal, thm) Hashtbl.t;
+}
+
+let create_session () =
+  { goal_stack = []; proof_script = []; cache = Hashtbl.create 10 }
+
+(* Handler for running tactics *)
+let run_tactic session tactic goal =
   match tactic goal with
-  | effect (Subgoals gs), k ->
-      Printf.printf "Created %d subgoals\n" (List.length gs);
-      List.iteri (fun i g -> (Printf.printf "  %d: \n" i;  (print_term (gconcl g)))) gs;
-      
-      (* Solve each subgoal using commands *)
-      let thms = List.map (fun subgoal ->
-        (* Pop next command *)
-        match !commands with
-        | [] -> failwith "ran out of commands"
-        | Apply tac_name :: rest ->
-            commands := rest;
-            Printf.printf "Applying %s\n" (match tac_name with Assumption -> "assumption" | Conj -> "conj");
-            let tactic = get_tactic tac_name in
-            solve_with_commands commands tactic subgoal
-        | _ -> failwith "expected Apply command"
-      ) gs in
-      
-      continue k thms
-  | thm -> 
-      Printf.printf "Goal solved!\n";
+  | effect Subgoals gs, k ->
+      Printf.printf "Tactic needs subgoals: %d\n" (List.length gs);
+      List.iteri
+        (fun i g ->
+          Printf.printf "  %d: " i;
+          print_term (gconcl g))
+        gs;
+
+      (* Check if all subgoals are in cache *)
+      let all_cached = List.for_all (fun g -> Hashtbl.mem session.cache g) gs in
+
+      if all_cached then begin
+        Printf.printf "All subgoals cached, resuming\n";
+        let thms = List.map (fun g -> Hashtbl.find session.cache g) gs in
+        continue k thms
+      end
+      else begin
+        Printf.printf "Subgoals not ready, aborting\n";
+        (* Add unsolved subgoals to stack *)
+        let unsolved =
+          List.filter (fun g -> not (Hashtbl.mem session.cache g)) gs
+        in
+        session.goal_stack <- unsolved @ session.goal_stack;
+        raise Subgoals_not_ready
+      end
+  | thm ->
+      Printf.printf "Tactic completed directly\n";
       thm
 
-(* Top-level handler *)
-let run_commands (commands : command list) =
-  let commands_ref = ref commands in
-  let goal_ref = ref None in
-  
-  let rec process_commands () =
-    match !commands_ref with
-    | [] -> 
-        Printf.printf "All commands processed\n";
-        !goal_ref
-    | cmd :: rest ->
-        commands_ref := rest;
-        (match cmd with
-         | SetGoal g ->
-                 (print_endline "Set goal" ;
-                (print_term (gconcl g)));
-             goal_ref := Some g;
-             process_commands ()
-         | ShowGoal ->
-             (match !goal_ref with
-              | None -> Printf.printf "No goal set\n"
-              | Some g -> 
-                      (print_endline "Current goal: ";
-                      (print_term (gconcl g))));
-             process_commands ()
-         | Apply tac_name ->
-             (match !goal_ref with
-              | None -> failwith "no goal set"
-              | Some g ->
-                  Printf.printf "Applying %s to goal\n" (match tac_name with Assumption -> "assumption" | Conj -> "conj");
-                  let tactic = get_tactic tac_name in
-                  let thm = solve_with_commands commands_ref tactic g in
-                  (print_endline "Final theorem:";
-                  (print_thm thm));
-                  goal_ref := None;
-                  process_commands ()))
-  in
-  process_commands ()
+(* Version of run_tactic that doesn't modify the goal stack *)
+let run_tactic_no_stack_modification session tactic goal =
+  match tactic goal with
+  | effect Subgoals gs, k ->
+      let all_cached = List.for_all (fun g -> Hashtbl.mem session.cache g) gs in
+      if all_cached then begin
+        Printf.printf "All subgoals cached, resuming\n";
+        let thms = List.map (fun g -> Hashtbl.find session.cache g) gs in
+        continue k thms
+      end
+      else begin
+        Printf.printf "Subgoals not ready, aborting\n";
+        raise Subgoals_not_ready
+      end
+  | thm ->
+      Printf.printf "Tactic completed directly\n";
+      thm
+
+(* Try to complete goals by re-running tactics *)
+let retry_script session =
+  List.iter
+    (fun app ->
+      if not (Hashtbl.mem session.cache app.goal) then begin
+        Printf.printf "Retrying tactic on goal: ";
+        print_term (gconcl app.goal);
+        let tactic = get_tactic app.tactic in
+        try
+          let thm = run_tactic_no_stack_modification session tactic app.goal in
+          Printf.printf "SUCCESS! Goal completed:\n";
+          print_thm thm;
+          Hashtbl.add session.cache app.goal thm;
+          session.goal_stack <- List.filter (( <> ) app.goal) session.goal_stack
+        with Subgoals_not_ready -> Printf.printf "Still waiting on subgoals\n"
+      end)
+    session.proof_script
+
+(* Execute a command *)
+let exec_command session command =
+  match command with
+  | SetGoal g ->
+      print_endline "Set goal:";
+      print_term (gconcl g);
+      session.goal_stack <- [ g ]
+  | ShowGoal ->
+      Printf.printf "Goal stack (%d goals):\n" (List.length session.goal_stack);
+      List.iteri
+        (fun i g ->
+          Printf.printf "  %d: " i;
+          print_term (gconcl g))
+        session.goal_stack
+  | Apply tac_name -> (
+      match session.goal_stack with
+      | [] -> failwith "no goals to work on"
+      | goal :: rest -> (
+          Printf.printf "Applying %s\n"
+            (match tac_name with
+            | Assumption -> "assumption"
+            | Conj -> "conj"
+            | Refl -> "ref");
+
+          (* Update stack to remove current goal *)
+          session.goal_stack <- rest;
+
+          (* Add to proof script *)
+          let app = { tactic = tac_name; goal } in
+          session.proof_script <- session.proof_script @ [ app ];
+
+          (* Try to run it *)
+          let tactic = get_tactic tac_name in
+          try
+            let thm = run_tactic session tactic goal in
+            print_endline "Goal solved!";
+            print_thm thm;
+            Hashtbl.add session.cache goal thm;
+            retry_script session
+          with Subgoals_not_ready -> ()))
+
+let show_session session =
+  Printf.printf "\n=== Session State ===\n";
+  Printf.printf "Goal stack: %d\n" (List.length session.goal_stack);
+  List.iteri
+    (fun i g ->
+      Printf.printf "  %d: " i;
+      print_term (gconcl g))
+    session.goal_stack;
+  Printf.printf "Cached: %d\n" (Hashtbl.length session.cache);
+  Printf.printf "Script length: %d\n" (List.length session.proof_script);
+  Printf.printf "=====================\n\n"
 
 (* Test *)
-let p = make_var "p" bool_ty
-let q = make_var "q" bool_ty
+let test_interactive () =
+  let session = create_session () in
+  let p = make_var "p" bool_ty in
+  let q = make_var "q" bool_ty in
 
-(* Test: prove p ∧ q from assumptions p and q *)
-let test_commands = [
-  SetGoal ([p; q], make_conj p q);
-  Apply Conj;        (* splits into two subgoals: p and q *)
-  Apply Assumption;  (* solves first subgoal p *)
-  Apply Assumption;  (* solves second subgoal q *)
-]
+  print_endline "=== Step 1: Set goal ===";
+  exec_command session (SetGoal ([ p; q ], make_conj p q));
+  show_session session;
 
-let result = run_commands test_commands
+  print_endline "=== Step 2: Apply Conj ===";
+  exec_command session (Apply Conj);
+  show_session session;
+
+  print_endline "=== Step 3: Apply Assumption (first subgoal) ===";
+  exec_command session (Apply Assumption);
+  show_session session;
+
+  print_endline "=== Step 4: Apply Assumption (second subgoal) ===";
+  exec_command session (Apply Assumption);
+  show_session session
--- test/tactic_test.ml
+++ test/tactic_test.ml
@@ -1,39 +1,137 @@
 open Holinone
-(* open Elaborator *)
-(* open Parse *)
+open Elaborator
+open Parse
+open Derived
+
 (* open Printing *)
 open Tactic
 
-(* let%expect_test "basic" = *)
-(*   let () = Derived.reset () |> Result.get_ok in *)
-(*   let prg = *)
-(*     {| *)
-(* (theorem basic_refl *)
-(*     (fix ((x 'a)) *)
-(*         (= x x))) *)
-(*   |} *)
-(*   in *)
-(*   let ast = parse_string prg in *)
-(*   let tast = Tast.check_program ast in *)
-(*   let () = Elab.elab_program tast in *)
-(*   (!the_goals |> List.iter @@ fun (name, t) -> *)
-(*       print_endline name; *)
-(*       print_term t); *)
-(*   let goal  = List.assoc "basic_refl" !the_goals in *)
-(*   let cmds = [ASetGoal goal; APrintGoal] in *)
-(*   handler cmds; *)
-(**)
-(**)
-(**)
-(*   [%expect {| *)
-(*     basic_refl *)
-(*     x = x *)
-(*     |}] *)
 let%expect_test "basic" =
   let () = Derived.reset () |> Result.get_ok in
-  let _ = run_commands test_commands in
-  print_endline "done";
+  let prg = {|
+(type nat ()
+    (Z)
+    (S (nat)))
+(theorem basic_refl
+    (fix ((x nat) (y nat))
+        (/\ (= x x) (= y y))))
+  |} in
+  let ast = parse_string prg in
+  let tast = Tast.check_program ast in
+  let () = Elab.elab_program tast in
+  (* (!the_goals |> List.iter @@ fun (name, t) -> *)
+  (*     print_endline name; *)
+  (*     print_term t); *)
+  let goal = List.assoc "basic_refl" !the_goals in
+  let session = create_session () in
+  exec_command session (SetGoal ([], goal));
+  show_session session;
 
+  exec_command session (Apply Refl);
+  show_session session;
 
   [%expect {|
+    |}]
+
+let%expect_test "basic" =
+  let () = Derived.reset () |> Result.get_ok in
+  let session = create_session () in
+  let p = make_var "p" bool_ty in
+  let q = make_var "q" bool_ty in
+
+  print_endline "=== Step 1: Set goal ===";
+  exec_command session (SetGoal ([ p; q ], make_conj p q));
+  show_session session;
+
+  print_endline "=== Step 2: Apply Conj ===";
+  exec_command session (Apply Conj);
+  show_session session;
+
+  print_endline "=== Step 3: Apply Assumption (first subgoal) ===";
+  exec_command session (Apply Assumption);
+  show_session session;
+
+  print_endline "=== Step 4: Apply Assumption (second subgoal) ===";
+  exec_command session (Apply Assumption);
+  show_session session;
+
+  [%expect
+    {|
+    === Step 1: Set goal ===
+    Set goal:
+    p ∧ q
+
+
+    === Session State ===
+    Goal stack: 1
+      0: p ∧ q
+
+    Cached: 0
+    Script length: 0
+    =====================
+
+    === Step 2: Apply Conj ===
+    Applying conj
+    Tactic needs subgoals: 2
+      0: p
+
+      1: q
+
+    Subgoals not ready, aborting
+
+    === Session State ===
+    Goal stack: 2
+      0: p
+
+      1: q
+
+    Cached: 0
+    Script length: 1
+    =====================
+
+    === Step 3: Apply Assumption (first subgoal) ===
+    Applying assumption
+    Tactic completed directly
+    Goal solved!
+    p
+    ========================================
+    p
+
+    Retrying tactic on goal: p ∧ q
+
+    Subgoals not ready, aborting
+    Still waiting on subgoals
+
+    === Session State ===
+    Goal stack: 1
+      0: q
+
+    Cached: 1
+    Script length: 2
+    =====================
+
+    === Step 4: Apply Assumption (second subgoal) ===
+    Applying assumption
+    Tactic completed directly
+    Goal solved!
+    q
+    ========================================
+    q
+
+    Retrying tactic on goal: p ∧ q
+
+    All subgoals cached, resuming
+    Tactic completed directly
+    SUCCESS! Goal completed:
+    p
+    q
+    ========================================
+    p ∧ q
+
+
+    === Session State ===
+    Goal stack: 0
+    Cached: 3
+    Script length: 3
+    =====================
     |}]
