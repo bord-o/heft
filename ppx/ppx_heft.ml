59a61dbdfddfabde641b2c53fc400ae8379b2b50
Brody Little <brodylittle011@gmail.com>
2026-03-31 23:04:18 -0700

    fmt

--- ppx/ppx_heft.ml
+++ ppx/ppx_heft.ml
@@ -1,20 +1,18 @@
 open Ppxlib
 
-let convert  ~(loc:location) ~(path:label) (input : expression) =
+let convert ~(loc : location) ~(path : label) (input : expression) =
   let (module A) = Ast_builder.make loc in
   let _ = path in
   match input.pexp_desc with
   | Pexp_ident { txt = Lident name; _ } ->
-      (A.eapply
-        (A.evar ("String.length"))
-        [A.estring name])
-  | _ ->
-      Location.raise_errorf ~loc "expected an identifier"
- 
+      A.eapply (A.evar "String.length") [ A.estring name ]
+  | _ -> Location.raise_errorf ~loc "expected an identifier"
 
-let extension = Extension.declare "heft"  Extension.Context.expression Ast_pattern.(single_expr_payload __) convert
+let extension =
+  Extension.declare "heft" Extension.Context.expression
+    Ast_pattern.(single_expr_payload __)
+    convert
 
 let () =
   Driver.register_transformation "heft"
-    ~rules:[Context_free.Rule.extension extension]
-
+    ~rules:[ Context_free.Rule.extension extension ]
--- test/ppx_tests/ppx_test.ml
+++ test/ppx_tests/ppx_test.ml
@@ -1,7 +1,4 @@
-
 let%expect_test "basic transformation" =
   let f = [%heft test] in
   Printf.printf "%d" f;
   [%expect {| 4 |}]
-
-
